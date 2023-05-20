e8040c75668d77edec47a8d3355be9be59de9aeb
Jessada21 <133492895+Jessada21@users.noreply.github.com>
2023-05-21 00:07:01 +0700

    Add files via upload

---
+++ Homework2.1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;  
+public class Exercise33 
+{  
+    public static void Main()
+{
+    int no_row,c=1,blk,i,j;
+
+    no_row = Convert.ToInt32(Console.ReadLine());
+     if (no_row >= 0) {   
+    	
+    for(i=0;i<no_row;i++)
+    {
+        for(blk=1;blk<=no_row;blk++)
+        Console.Write("  ");
+        for(j=0;j<=i;j++)
+        {
+            if (j==0||i==0)
+                c=1;
+            else
+               c=c*(i-j+1)/j;
+            Console.Write("{0}   ",c);
+        }
+        Console.Write("\n"); 
+    }
+    } while (no_row < 0) {
+         Console.WriteLine("Invalid Pascal’s triangle row number.");
+          no_row = Convert.ToInt32(Console.ReadLine());
+          if (no_row >= 0) {   
+    	
+    for(i=0;i<no_row;i++)
+    {
+        for(blk=1;blk<=no_row;blk++)
+        Console.Write("  ");
+        for(j=0;j<=i;j++)
+        {
+            if (j==0||i==0)
+                c=1;
+            else
+               c=c*(i-j+1)/j;
+            Console.Write("{0}   ",c);
+        }
+        Console.Write("\n"); 
+    }
+    
+  }	
+}
+}
+}
